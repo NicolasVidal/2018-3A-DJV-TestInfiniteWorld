77be8b0c3d853f2adbfe158f9c703b26bf48299e
Nicolas Vidal <nicolas.vidal@iscool-e.com>
2018-05-28 14:37:22 +0200

    Monde de l'enfer !

---
+++ Assets/TerrainManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManagerScript : MonoBehaviour
+{
+	[SerializeField]
+	private Terrain terrain;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	void GenerateTerrain()
+	{
+		var resolution = terrain.terrainData.heightmapResolution;
+		var heights = new float[resolution, resolution];
+
+		for (var i = 0; i < resolution; i++)
+		{
+			for (var j = 0; j < resolution; j++)
+			{
+				heights[j, i] = Random.Range(0f, 1f);
+			}
+		}
+		
+		terrain.terrainData.SetHeights(0, 0, heights);
+	}
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GenerateTerrain();
+		}
+	}
+}
