b719f7b0b4a8ac8462eda82e22f8b193c30b7198
Nicolas Vidal <nicolas.vidal@iscool-e.com>
2018-05-28 16:08:08 +0200

    The World is generating as player move, but it is freezing

--- Assets/TerrainManagerScript.cs
+++ Assets/TerrainManagerScript.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TerrainManagerScript : MonoBehaviour
 {
     [SerializeField]
-    private Terrain terrain;
+    private Terrain[] terrains;
 
     [SerializeField]
     private int gaussianPerTerrainCount = 9;
@@ -22,56 +24,106 @@ public class TerrainManagerScript : MonoBehaviour
     [SerializeField]
     private float gammaMaxValue;
 
+    [SerializeField]
+    private Transform playerTransform;
+
     // Use this for initialization
     void Start()
     {
     }
 
-    void GenerateTerrain()
+    Vector2 IdToOffset(int id)
     {
-        var resolution = terrain.terrainData.heightmapResolution;
-        var heights = new float[resolution, resolution];
-
-
-        Random.InitState(globalSeed);
-
-        var centers = new Vector2[gaussianPerTerrainCount];
-        var gamma = new float[gaussianPerTerrainCount];
-
-        for (var g = 0; g < gaussianPerTerrainCount; g++)
+        switch (id)
         {
-            centers[g] = new Vector2(Random.Range(0f, terrainSize), Random.Range(0f, terrainSize));
-            gamma[g] = Random.Range(0f, terrainSize);
+            case 0 : return new Vector2(-1f, -1f);
+            case 1 : return new Vector2(0f, -1f);
+            case 2 : return new Vector2(1f, -1f);
+            case 3 : return new Vector2(-1f, 0f);
+            case 4 : return new Vector2(0f, 0f);
+            case 5 : return new Vector2(1f, 0f);
+            case 6 : return new Vector2(-1f, 1f);
+            case 7 : return new Vector2(0f, 1f);
+            case 8 : return new Vector2(1f, 1f);
+            default: throw new Exception();
         }
+    }
 
+    void GenerateTerrain(float x, float z)
+    {
+        var centers = new Vector2[terrains.Length][];
+        var gammas = new float[terrains.Length][];
 
-        for (var i = 0; i < resolution; i++)
+        for (var id = 0; id < terrains.Length; id++)
         {
-            for (var j = 0; j < resolution; j++)
+            var coordinates = IdToOffset(id) * terrainSize;
+
+            Random.InitState(globalSeed + Mathf.RoundToInt(x + coordinates.x) + Mathf.RoundToInt((z + coordinates.y) * 20000));
+            centers[id] = new Vector2[gaussianPerTerrainCount];
+            gammas[id] = new float[gaussianPerTerrainCount];
+
+            for (var g = 0; g < gaussianPerTerrainCount; g++)
             {
-                var height = 0f;
-
-                for (var g = 0; g < gaussianPerTerrainCount; g++)
-                {
-                    height += Mathf.Exp(-gamma[g]
-                                        * Vector2.SqrMagnitude(centers[g] / terrainSize - new Vector2(i, j) / resolution
-                                        ));
-                }
-
-                heights[j, i] = height / gaussianPerTerrainCount;
+                centers[id][g] = new Vector2(Random.Range(0f, terrainSize), Random.Range(0f, terrainSize));
+                gammas[id][g] = Random.Range(gammaMinValue, gammaMaxValue);
             }
         }
+        
+        for (var id = 0; id < terrains.Length; id++)
+        {
+            var resolution = terrains[id].terrainData.heightmapResolution;
+            var heights = new float[resolution, resolution];
+            
+            var coordinates = IdToOffset(id) * terrainSize;
 
-        terrain.terrainData.SetHeights(0, 0, heights);
+            for (var i = 0; i < resolution; i++)
+            {
+                for (var j = 0; j < resolution; j++)
+                {
+                    var height = 0f;
+                    
+                    for (var id2 = 0; id2 < terrains.Length; id2++)
+                    {
+                        var coordinates2 = IdToOffset(id2) * terrainSize;
+                        
+                        for (var g = 0; g < gaussianPerTerrainCount; g++)
+                        {
+                            var vertexCoordinate = new Vector2(i, j) / resolution * terrainSize;
+                            vertexCoordinate = new Vector2(Mathf.Ceil(vertexCoordinate.x), Mathf.Round(vertexCoordinate.y));
+                            
+                            
+                            height += Mathf.Exp(-gammas[id2][g]
+                                                * Vector2.SqrMagnitude(
+                                                    (centers[id2][g] + coordinates2) / terrainSize - 
+                                                    (vertexCoordinate + coordinates) / terrainSize)
+                                                );
+                        }
+                    }
+
+                    heights[j, i] = height / gaussianPerTerrainCount / 9f;
+                }
+            }
+
+            terrains[id].transform.position = new Vector3(x + coordinates.x, 0f, z + coordinates.y);
+            terrains[id].terrainData.SetHeights(0, 0, heights);
+        }
     }
 
 
+    private int lastX = int.MinValue;
+    private int lastZ = int.MinValue;
+    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        var x = Mathf.FloorToInt(playerTransform.position.x / terrainSize);
+        var z = Mathf.FloorToInt(playerTransform.position.z / terrainSize);
+        
+        if (lastX != x || lastZ != z)
         {
-            GenerateTerrain();
+            lastX = x;
+            lastZ = z;
+            GenerateTerrain(x * terrainSize, z * terrainSize);
         }
     }
 }
