04dc7e2e82f1bc1db58ff163cf131d76912d2426
Nicolas Vidal <nicolas.vidal@iscool-e.com>
2018-05-28 14:49:44 +0200

    Terrain is now generated with gaussians ! :)

--- Assets/TerrainManagerScript.cs
+++ Assets/TerrainManagerScript.cs
@@ -4,37 +4,74 @@ using UnityEngine;
 
 public class TerrainManagerScript : MonoBehaviour
 {
-	[SerializeField]
-	private Terrain terrain;
+    [SerializeField]
+    private Terrain terrain;
 
-	// Use this for initialization
-	void Start ()
-	{
-	}
+    [SerializeField]
+    private int gaussianPerTerrainCount = 9;
 
-	void GenerateTerrain()
-	{
-		var resolution = terrain.terrainData.heightmapResolution;
-		var heights = new float[resolution, resolution];
+    [SerializeField]
+    private int globalSeed = 42;
 
-		for (var i = 0; i < resolution; i++)
-		{
-			for (var j = 0; j < resolution; j++)
-			{
-				heights[j, i] = Random.Range(0f, 1f);
-			}
-		}
-		
-		terrain.terrainData.SetHeights(0, 0, heights);
-	}
-	
-	
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			GenerateTerrain();
-		}
-	}
+    [SerializeField]
+    private float terrainSize;
+
+    [SerializeField]
+    private float gammaMinValue;
+
+    [SerializeField]
+    private float gammaMaxValue;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void GenerateTerrain()
+    {
+        var resolution = terrain.terrainData.heightmapResolution;
+        var heights = new float[resolution, resolution];
+
+
+        Random.InitState(globalSeed);
+
+        var centers = new Vector2[gaussianPerTerrainCount];
+        var gamma = new float[gaussianPerTerrainCount];
+
+        for (var g = 0; g < gaussianPerTerrainCount; g++)
+        {
+            centers[g] = new Vector2(Random.Range(0f, terrainSize), Random.Range(0f, terrainSize));
+            gamma[g] = Random.Range(0f, terrainSize);
+        }
+
+
+        for (var i = 0; i < resolution; i++)
+        {
+            for (var j = 0; j < resolution; j++)
+            {
+                var height = 0f;
+
+                for (var g = 0; g < gaussianPerTerrainCount; g++)
+                {
+                    height += Mathf.Exp(-gamma[g]
+                                        * Vector2.SqrMagnitude(centers[g] / terrainSize - new Vector2(i, j) / resolution
+                                        ));
+                }
+
+                heights[j, i] = height / gaussianPerTerrainCount;
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heights);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateTerrain();
+        }
+    }
 }
